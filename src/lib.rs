b4512e8406c3c8e428dc273c31e6b7a06684acac
Tom Gehrke <phyrog@gmail.com>
2021-02-28 00:13:08 +0100

    Change enum and constants types

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,10 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub const CANARD_MTU_CAN_CLASSIC: size_t = 8;
+pub const CANARD_MTU_CAN_FD: size_t = 64;
+pub const CANARD_NODE_ID_MAX: CanardNodeID = 127;
+pub const CANARD_NODE_ID_UNSET: CanardNodeID = 255;
+pub const CANARD_PRIORITY_MAX: CanardPriority = CanardPriority::CanardPriorityOptional;
+pub const CANARD_TRANSFER_ID_MAX: CanardTransferID = 31;
